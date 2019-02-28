491264ae89c0478997a7b34657df848be652aaa3
yzf <865846263@qq.com>
2019-02-28 17:19:56 +0800

    添加Test04

---
+++ SourceTreeTest02/src/main/java/com/yzf/test/Test04.java
@@ -0,0 +1,11 @@
+package main.java.com.yzf.test;
+
+/**
+ * @author yzf
+ * @date 2019-02-28 17:18
+ */
+public class Test04 {
+    public static void main(String[] args) {
+        System.out.println("Test04");
+    }
+}
